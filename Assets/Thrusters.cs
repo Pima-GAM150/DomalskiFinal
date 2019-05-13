307b9c5930c1ca139b7e7a81dda1c1c18df3cf38
Evolved Bag <undercloudstudio@gmail.com>
2019-05-12 22:39:10 -0700

    starting work on movement

---
+++ Assets/Thrusters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Thrusters : MonoBehaviour
+{
+    
+    public float verticalRot, horizontalRot, baseForce, thrust, rotRate, thrustRate;
+    public Text	 v, h, t;
+
+    void Update()
+    {
+        
+    	v.text = "" +verticalRot;
+    	h.text = "" + horizontalRot;
+    	t.text = "" + (100 * thrust);
+
+    	if(Input.GetKey(KeyCode.shift))
+
+    }
+}
