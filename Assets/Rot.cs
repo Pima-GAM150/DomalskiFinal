e9e38c7b778ec3c36cd8384dfd5966feeb2c1bf4
Evolved Bag <undercloudstudio@gmail.com>
2019-05-14 22:41:44 -0700

    Rotating Guide Arrow

---
+++ Assets/Rot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rot : MonoBehaviour
+{
+   	
+   	public float speed;
+
+    void Update()
+    {
+        
+    	transform.Rotate(0f, 0f, speed * Time.deltaTime);
+
+    }
+}
